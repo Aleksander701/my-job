c3c0ef2648f8fd569ed727744651acbbe07c367c
geekbrains <phantom335@mail.ru>
2020-09-15 20:38:38 +0300

    my-job

---
+++ mysite/views/article_admin_edit.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf8">
+    <title> My Blog</title>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+    <div class="fon">
+        <h1>Выполняемые заказы</h1>
+        <div>
+            <form method="post" action="index.php?action=<?=$_GET['action']?>&id=<?=$_GET['id']?>">
+                <div>
+                    <label> Название<br> <input type="text" name="title" value="<?=$article['title']?>" class="form=item" autofocus required> 
+                    </label>
+                </div>
+                
+                <div>
+                    <label> Дата<br> <input type="date" name="date" value="<?=$article['date']?>" class="form=item" required>
+                    </label>
+                </div>
+                
+                <div>
+                    <label> Содержимое<br><textarea class="form=item" name="content" required><?=$article['content']?></textarea>
+                    </label>
+                </div>
+                <input type="submit" value="Сохранить" class="btn">
+            </form>
+            </div>
+        </div>
+        <footer>
+            <p>Блог<br>Copyright &copy; 2017</p>
+        </footer>
+    </div>       
+</body>
+</html>
+    
---
+++ mysite/views/articles_admin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> My Blog</title>
+        <link rel="stylesheet" href="../style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <h1>Выполняемые заказы</h1>
+            <div>
+                <a href="index.php?action=add">Добавить заказ</a>
+                <a href="http://localhost/mysite/index.php">Назад</a>
+                <table class = "admin-table">
+                    <tr>
+                        <th> ДАТА </th>
+                        <th> ЗАГОЛОВОК </th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <?php foreach($articles as $a): ?>
+                    <tr>
+                        <td> <?=$a['date']?> </td>
+                        <td> <?=$a['title']?></td>
+                        <td> <a href="index.php?action=edit&id=<?=$a['id']?>"> Редактировать </a></td>
+                         <td> <a href="index.php?action=delete&id=<?=$a['id']?>"> Удалить </a></td>
+                    </tr>
+                    <?php endforeach ?>
+                </table>
+            </div>
+            <footer>
+                <p>заказы<br>Copyright &copy; 2017</p>
+            </footer>
+        </div>
+    </body>
+</html>
